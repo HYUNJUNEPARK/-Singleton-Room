821cd061d46bf6f63ffaad2704f426b34b4d872c
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-09-06 13:31:51 +0900

    animation 적용

--- app/src/main/java/com/example/room/anim/RotateExpandAnimation.kt
+++ app/src/main/java/com/example/room/anim/RotateExpandAnimation.kt
@@ -1,30 +1,27 @@
 package com.example.room.anim
 
-import android.graphics.drawable.Animatable
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.Transformation
 
-class ToggleAnimation {
+class RotateExpandAnimation {
     companion object {
-        fun toggleArrow(view: View, isExpanded: Boolean): Boolean {
-            return if (isExpanded) { //서브 뷰가 펼쳐져 있고 화살표는 아래로 향한 상태
-                view.animate().setDuration(200).rotation(180f)
+        fun rotateArrow(view: View, isExpanded: Boolean): Boolean {
+            return if (isExpanded) { //서브 뷰가 펼쳐져 있고 화살표는 아래로 향한 상태(기본 설정: 화살표 위로)
+                view.animate().setDuration(200).rotation(0f) //화살표를 위로 돌린다.
                 true
-            } else { //서브 뷰가 접혀 있고 화살표는 위로 향한 상태
-                view.animate().setDuration(200).rotation(0f)
+            } else { //서브 뷰가 접혀 있고 화살표는 위로 향한 상태(기본 설정: 화살표 위로)
+                view.animate().setDuration(200).rotation(180f) //화살표를 아래로 돌린다.
                 false
             }
         }
 
-        //접힘 -> 펼침
         fun expand(view: View) {
             val animation = expandAction(view)
             view.startAnimation(animation)
         }
 
-        //애니메이션 적용 시간
         private fun expandAction(view:View): Animation {
             view.measure(
                 /*widthMeasureSpec*/ViewGroup.LayoutParams.MATCH_PARENT,
